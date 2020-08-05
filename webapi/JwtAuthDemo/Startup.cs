d1c46368f26004fcd6bc742efab1ad659bd19dc7
Changhui Xu <changhuixu@hotmail.com>
2020-08-05 09:27:10 -0500

    add Swagger OpenAPI Security Scheme

--- webapi/JwtAuthDemo/Startup.cs
+++ webapi/JwtAuthDemo/Startup.cs
@@ -56,6 +56,26 @@ namespace JwtAuthDemo
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "JWT Auth Demo", Version = "v1" });
+
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Name = "JWT Authentication",
+                    Description = "Enter JWT Bearer token **_only_**",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer", // must be lower case
+                    BearerFormat = "JWT",
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+                c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {securityScheme, new string[] { }}
+                });
             });
 
             services.AddCors(options =>
